467c631fbf7cd0d11e69e636daaabc6cf7affb30
IbnuUbay <ibnuubaidillah12@student.polindra.ac.id>
2023-05-22 11:36:48 +0700

    first commit

---
+++ biodata.js
@@ -0,0 +1,15 @@
+exports.getNama = function (){
+    return "Ibnu Ubadillah";
+};
+
+exports.getTempatLahir = function (){
+    return "Indramayu";
+}
+
+exports.getTanggalLahir = function (){
+    return "12 Juli 2002";
+}
+
+exports.getAlamat = function (){
+    return "Jl. Posyandu No.124";
+};
---
+++ moduleBiodata.js
@@ -0,0 +1,5 @@
+const biodata = require("./biodata");
+
+console.log("Nama: " + biodata.getNama());
+console.log("Tempat Lahir: " + biodata.getTempatLahir());
+console.log("Alamat: " + biodata.getAlamat());
